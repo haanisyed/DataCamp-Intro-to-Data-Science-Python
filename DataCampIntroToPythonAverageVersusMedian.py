e29eaa2e49a7d8faca56e0f7e87b15ae03391aec
Haani <116673121+haanisyed@users.noreply.github.com>
2023-02-25 21:40:46 -0500

    Add files via upload
    You now know how to use numpy functions to get a better feeling for your data. It basically comes down to importing numpy and then calling several simple functions on the numpy arrays:
    
    import numpy as np
    x = [1, 4, 8, 10, 12]
    np.mean(x)
    np.median(x)
    The baseball data is available as a 2D numpy array with 3 columns (height, weight, age) and 1015 rows. The name of this numpy array is np_baseball. After restructuring the data, however, you notice that some height values are abnormally high. Follow the instructions and discover which summary statistic is best suited if you're dealing with so-called outliers.
    Create numpy array np_height_in that is equal to first column of np_baseball.
    Print out the mean of np_height_in.
    Print out the median of np_height_in.

---
+++ DataCampIntroToPythonAverageVersusMedian.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # np_baseball is available
+... 
+... # Import numpy
+... import numpy as np
+... 
+... # Create np_height_in from np_baseball
+... np_height_in = np_baseball[:,0]
+... 
+... # Print out the mean of np_height_in
+... print(np.mean(np_height_in))
+... 
+... # Print out the median of np_height_in
